45fcbfa34f69349330d31710babcce5dea89a4b8
flsilves <flavio.silvestre@nokia.com>
2017-07-17 17:24:28 +0100

    Fix

--- inotify.cc
+++ inotify.cc
@@ -55,6 +55,8 @@ void folder_listener(int inotify_instance) {
     
 
     while(1) {
+
+        cout << "Inotify listener: waiting for events" << endl;
         num_read = read(inotify_instance, buf, BUF_LEN);
 
         if (num_read == 0) {
@@ -66,36 +68,41 @@ void folder_listener(int inotify_instance) {
             printf("FATAL: read rom inotify fd returned -1!");
             exit(EXIT_FAILURE);
         }
-
-        printf("Read %ld bytes from inotify fd\n", (long) num_read);
+        
+        printf("Inotify listener: Aquiring lock\n");
+        unique_lock<mutex> lk(cv_m);
+        printf("Inotify listener: Read %ld bytes from inotify fd\n", (long) num_read);
 
         for (buffer_pointer = buf; buffer_pointer < buf + num_read;   ) {
             event = (struct inotify_event *) buffer_pointer;
 
-            unique_lock<mutex> lk(cv_m);
+
             file_list.insert(event->name);
-            print_container();
-            cv.notify_one();
+    
+
+            
             buffer_pointer += sizeof (struct inotify_event) +event->len;
         }
+        printf("Inotify listener: notifying\n");
+        cv.notify_one();
+        print_container();
     }
 }
 
 void consume_files(){
     
-    cout << "Consumer thread" << endl;
     while(1)
     {   
         unique_lock<mutex> lk(cv_m);
-        cout << "Waiting... \n";
+        cout << "Consumer thread waiting... \n";
         cv.wait(lk, []{return file_list.size() !=0 ;});
 
-        cout << "Consumer size " << file_list.size() << endl;
+        cout << "Consumer thread: backlog size " << file_list.size() << endl;
         auto it = file_list.begin();
         while(it != file_list.end())
         {
                 file_list.erase(it);
-                cout << "Removed item from list" << endl;
+                cout << "Consumer thread: Removed item from list" << endl;
                 it++;
                 usleep(1000000);
        }
