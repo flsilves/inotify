9a3cd8b08d9bbe0893d07944f06b551e4bb20416
flsilves <flavio.silvestre@nokia.com>
2017-09-22 09:47:45 +0100

    SAVEPOINT

---
+++ cmake-build-debug/CMakeFiles/inotify.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inotify.dir/inotify.cc.o"
+  "inotify.pdb"
+  "inotify"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inotify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
