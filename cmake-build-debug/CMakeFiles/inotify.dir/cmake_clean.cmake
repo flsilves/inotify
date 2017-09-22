2a586a3e0fe27cb35eec5deb6253f6b8b1ec2c2d
flsilves <flavio.silvestre@nokia.com>
2017-09-22 15:53:39 +0100

    SAVEPOINT

---
+++ cmake-build-debug/CMakeFiles/inotify.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inotify.dir/listener.cpp.o"
+  "CMakeFiles/inotify.dir/backlog.cpp.o"
+  "CMakeFiles/inotify.dir/inotify.cpp.o"
+  "CMakeFiles/inotify.dir/myTimer.cpp.o"
+  "inotify.pdb"
+  "inotify"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inotify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
